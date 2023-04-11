f43d89b9ab4f7af73d5c81ed4766068379507970
Obinna <obinnaogbu80@gmail.com>
2023-04-11 21:37:44 +0000

    updated

--- 0x0A-python-inheritance/6-base_geometry.py
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -6,4 +6,4 @@ class BaseGeometry:
     """initialization of BaseGeometry"""
     def area(self):
         """public instance method (area)"""
-        raise Exception("area() is not implemented"
+        raise Exception("area() is not implemented")
